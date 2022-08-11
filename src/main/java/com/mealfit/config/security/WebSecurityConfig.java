fee2bbc5f0c51f93ecd0940860fd6e1772c1c9a7
Jiyoung Baek <vvvv4449@gmail.com>
2022-08-12 01:21:11 +0900

    #5 로그인 기능 구현 수정중(secretKey 숨김처리)

--- src/main/java/com/mealfit/config/security/WebSecurityConfig.java
+++ src/main/java/com/mealfit/config/security/WebSecurityConfig.java
@@ -2,16 +2,58 @@ package com.mealfit.config.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import com.mealfit.loginJwtSocial.UserRepository;
+import com.mealfit.loginJwtSocial.jwtFilter.FormLoginFilter;
+import com.mealfit.loginJwtSocial.jwtFilter.FormLoginProvider;
+import com.mealfit.loginJwtSocial.jwtFilter.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.web.cors.CorsUtils;
-
+@RequiredArgsConstructor
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final FormLoginProvider formLoginProvider;
+    private final UserRepository userRepository;
+
+    @Bean   // 비밀번호 암호화
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override // Bean 에 등록
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    // 정적 자원에 대해서는 Security 설정을 적용하지 않음.
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
+
+//    @Override // ignore check swagger resource  // 시도해봤는데 webseurity web 빨간줄뜸.
+//    public void configure(WebSecurity web) {
+//        web.ignoring()
+//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+//                .antMatchers("/v2/api-docs", "/swagger-resources/**",
+//                        "/swagger-ui.html", "/swagger-ui/**", "/webjars/**", "/swagger/**");
+//    }
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,12 +71,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
               .authorizeRequests(request -> request
                     .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                    .anyRequest().permitAll());
+                    .anyRequest().permitAll())
+
+                .addFilterBefore(new FormLoginFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthorizationFilter(authenticationManager(), userRepository), UsernamePasswordAuthenticationFilter.class)
+        ;
+
     }
 
-    @Override // ignore check swagger resource
+    @Override // ignore check swagger resource  // 합쳤는데, 바꿔도 되나요?
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**",
-              "/swagger-ui.html", "/swagger-ui/**", "/webjars/**", "/swagger/**");
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .antMatchers("/v2/api-docs", "/swagger-resources/**",
+                        "/swagger-ui.html", "/swagger-ui/**", "/webjars/**", "/swagger/**");
     }
 }
