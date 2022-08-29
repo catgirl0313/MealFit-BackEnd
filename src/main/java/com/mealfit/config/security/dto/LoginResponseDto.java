ae00d797ffbfa926ddbee9074ed9dc798905d8e2
Jiyoung Baek <vvvv4449@gmail.com>
2022-08-29 20:56:46 +0900

    userstatus 추가

--- src/main/java/com/mealfit/config/security/dto/LoginResponseDto.java
+++ src/main/java/com/mealfit/config/security/dto/LoginResponseDto.java
@@ -2,6 +2,7 @@ package com.mealfit.config.security.dto;
 
 import com.mealfit.config.security.details.UserDetailsImpl;
 import com.mealfit.user.domain.User;
+import com.mealfit.user.domain.UserStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,14 +17,23 @@ public class LoginResponseDto {
     private TokenBox tokenBox;
     private UserInfoDto userInfoDto;
 
-    public LoginResponseDto(String accessToken, String refreshToken,
-          UserDetailsImpl userDetails) {
+    public LoginResponseDto(String accessToken, String refreshToken, UserDetailsImpl userDetails) {
         this.tokenBox = new TokenBox(accessToken, refreshToken);
-        this.userInfoDto = new UserInfoDto(userDetails.getUser());
+        this.userInfoDto = new UserInfoDto(userDetails.getNickName(), userDetails.getProfile(), userDetails.getUser().getUserStatus());
     }
+//    public LoginResponseDto(String accessToken, String refreshToken,
+//                            UserDetailsImpl userDetails, UserStatus userStatus) {
+//        this.tokenBox = new TokenBox(accessToken, refreshToken);
+//        this.userInfoDto = new UserInfoDto(userDetails.getUser(), userStatus);
+//    }
 
+//    public LoginResponseDto(String accessToken, String refreshToken,
+//          String nickname, String profile, UserStatus userStatus) {
+//        this.tokenBox = new TokenBox(accessToken, refreshToken);
+//        this.userInfoDto = new UserInfoDto(nickname, profile, userStatus);
+//    }
     public LoginResponseDto(String accessToken, String refreshToken,
-          String nickname, String profile) {
+                            String nickname, String profile) {
         this.tokenBox = new TokenBox(accessToken, refreshToken);
         this.userInfoDto = new UserInfoDto(nickname, profile);
     }
@@ -45,11 +55,38 @@ public class LoginResponseDto {
 
         private String nickname;
         private String profile;
+        private UserStatus userStatus;
 
         public UserInfoDto(User user) {
+            this.nickname = user.getNickname();
+            this.profile = user.getProfileImage();
+            this.userStatus = user.getUserStatus();
+
+        }
+
+        public UserInfoDto(String nickname, String profile) {
             this.nickname = nickname;
             this.profile = profile;
+
+
         }
+
+//
+//        public UserInfoDto(User user, UserStatus userStatus) {
+//            this.nickname = user.getNickname();
+//            this.profile = user.getProfileImage();
+//            this.userStatus = userStatus;
+//        }
+//        public UserInfoDto(User user) {
+//            this.nickname = user.getNickname();
+//            this.profile = user.getProfileImage();
+//        }
+//
+//        public UserInfoDto(String user, String userStatus) {
+//            this.nickname = nickname;
+//            this.profile = profile;
+//            this.userStatus = userStatus;
+//        }
     }
 
 }
